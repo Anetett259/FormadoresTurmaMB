ed1e8ccbf633d3a389f1be7a15824c3205f50aaf
Anete Terezinha Trasel <anete.trasel@escola.pr.gov.br>
2025-07-03 12:07:08 +0000

    aula 0307

---
+++ script.js
@@ -0,0 +1,14 @@
+function inserirNome(){
+ let nomeUsuario = prompt("Qual o seu nome");
+ let elemento = document.querySelector("#nome-usuario");
+ elemento.textContent = nomeUsuario;
+}
+inserirNome();
+
+let linguagens = ["JavaScript","Python","C"];
+
+
+const item = document.querySelector("lista");
+item.textContent= linguagens[1];
+
+
