b996065292d2f8b8f5ce530f86ded76294eeedb8
Anete Terezinha Trasel <anete.trasel@escola.pr.gov.br>
2025-07-31 08:33:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function inserirNome(){
- let nomeUsuario = prompt("Qual o seu nome");
- let elemento = document.querySelector("#nome-usuario");
- elemento.textContent = nomeUsuario;
+   let nomeUsuario = prompt("Qual o seu nome");
+   let elemento = document.querySelector("#nome-usuario");
+   elemento.textContent = nomeUsuario;
 }
+
 inserirNome();
 
-let linguagens = ["JavaScript","Python","C"];
+//Lista de linguagens escritas:
+const linguagens = ["JavaScript", "Python", "C++"]
+
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1]) // “Python”
+console.log(linguagens[2]) // “C++”
 
 
-const item = document.querySelector("lista");
-item.textContent= linguagens[1];
-
-
+// Adicionar valores da lista na página com DOM.
+const lista = document.querySelector("#lista");  //seleciona elemento com id "lista" e guarda na variável "lista"
+lista.textContent = linguagens[0]; // altera o elemento HTML da variável "lista" para o valor 0 da lista "linguagens"
+lista.textContent = linguagens[1]; 
+lista.textContent = linguagens[2]; 
